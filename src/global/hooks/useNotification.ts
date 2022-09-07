38d62ed3bab21c84d5678afc2fdeb209c3699882
Kevin Mason <kevin.mason@valr.com>
2022-09-07 20:06:46 +0200

    removed logic preventing notifications

--- src/global/hooks/useNotification.ts
+++ src/global/hooks/useNotification.ts
@@ -1,45 +1,42 @@
 import { useToast } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-
-let firstRender = true;
+import { useEffect } from "react";
 
 const useNotification = (showNotification: boolean) => {
   const toast = useToast();
   useEffect(() => {
-    if (firstRender) {
-      try {
-        if (!("Notification" in window)) {
-          alert("This browser does not support desktop notification");
-        } else if (Notification.permission === "granted") {
-          if (showNotification) {
+    try {
+      if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+      } else if (Notification.permission === "granted") {
+        if (showNotification) {
+          navigator.serviceWorker.ready.then(function (registration) {
+            registration.showNotification("Notification with ServiceWorker");
+          });
+          new Notification(
+            "All balls have been dropped from the first section"
+          );
+        }
+      } else if (Notification.permission !== "denied") {
+        // Ask the user for permission
+        Notification.requestPermission().then((permission) => {
+          // If the user accepts, let's create a notification
+          if (permission === "granted") {
             navigator.serviceWorker.ready.then(function (registration) {
               registration.showNotification("Notification with ServiceWorker");
             });
-            new Notification(
-              "All balls have been dropped from the first section"
-            );
+            //   new Notification(
+            //     "Hi there! you will receive notifications like this one."
+            //   );
           }
-        } else if (Notification.permission !== "denied") {
-          // Ask the user for permission
-          Notification.requestPermission().then((permission) => {
-            // If the user accepts, let's create a notification
-            if (permission === "granted") {
-              navigator.serviceWorker.ready.then(function (registration) {
-                registration.showNotification(
-                  "Notification with ServiceWorker"
-                );
-              });
-              new Notification(
-                "Hi there! you will receive notifications like this one."
-              );
-            }
-          });
-        }
-      } catch (error) {
-        toast({ title: "Whoops something went wrong with notifications!" });
+        });
       }
+    } catch (error: any) {
+      toast({
+        title: `Whoops something went wrong with notifications! errorMessage: ${JSON.stringify(
+          error
+        )}`,
+      });
     }
-    firstRender = false;
   }, [showNotification]);
 };
 
