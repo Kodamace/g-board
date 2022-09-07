3e1b82542951de7d2647f11164b3376edd1d1d8f
Kevin Mason <kevin.mason@valr.com>
2022-09-07 19:04:03 +0200

    testing/service-workers

--- src/global/hooks/useNotification.ts
+++ src/global/hooks/useNotification.ts
@@ -1,33 +1,45 @@
 import { useToast } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+let firstRender = true;
 
 const useNotification = (showNotification: boolean) => {
   const toast = useToast();
   useEffect(() => {
-    try {
-      if (!("Notification" in window)) {
-        alert("This browser does not support desktop notification");
-      } else if (Notification.permission === "granted") {
-        if (showNotification) {
-          new Notification(
-            "All balls have been dropped from the first section"
-          );
-        }
-      } else if (Notification.permission !== "denied") {
-        // Ask the user for permission
-        Notification.requestPermission().then((permission) => {
-          // If the user accepts, let's create a notification
-          if (permission === "granted") {
-            // new Notification(
-            //   "Hi there! you will receive notifications like this one."
-            // );
-            // …
+    if (firstRender) {
+      try {
+        if (!("Notification" in window)) {
+          alert("This browser does not support desktop notification");
+        } else if (Notification.permission === "granted") {
+          if (showNotification) {
+            navigator.serviceWorker.ready.then(function (registration) {
+              registration.showNotification("Notification with ServiceWorker");
+            });
+            new Notification(
+              "All balls have been dropped from the first section"
+            );
           }
-        });
+        } else if (Notification.permission !== "denied") {
+          // Ask the user for permission
+          Notification.requestPermission().then((permission) => {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+              navigator.serviceWorker.ready.then(function (registration) {
+                registration.showNotification(
+                  "Notification with ServiceWorker"
+                );
+              });
+              new Notification(
+                "Hi there! you will receive notifications like this one."
+              );
+            }
+          });
+        }
+      } catch (error) {
+        toast({ title: "Whoops something went wrong with notifications!" });
       }
-    } catch (error) {
-      toast({ title: "Whoops something went wrong with notifications!" });
     }
+    firstRender = false;
   }, [showNotification]);
 };
 
