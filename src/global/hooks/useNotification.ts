36c83014579fe37b83bfaab5bed88fee8c71a30e
Kevin Mason <kevin.mason@valr.com>
2022-09-08 16:17:16 +0200

    update push notification hook

--- src/global/hooks/useNotification.ts
+++ src/global/hooks/useNotification.ts
@@ -1,16 +1,28 @@
-import { useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
+// @ts-ignore
+import notificationSound from "../../../src/assets/notification-sound-7062.mp3";
 
-const useNotification = (showNotification: boolean) => {
+const useNotification = (
+  showNotification: boolean,
+  title: string,
+  sound?: any,
+  icon?: string,
+  body?: string
+) => {
+  const options: any = {
+    body,
+    icon,
+    sound,
+  };
   useEffect(() => {
     try {
       if (!("Notification" in window)) {
         alert("This browser does not support desktop notification");
       } else if (Notification.permission === "granted") {
         if (showNotification) {
-          new Notification(
-            "All balls have been dropped from the first section"
-          );
+          new Notification(title, options);
+          const audio = new Audio(notificationSound);
+          audio.play();
         }
       } else if (Notification.permission !== "denied") {
         // Ask the user for permission
